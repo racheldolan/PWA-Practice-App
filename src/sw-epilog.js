9626720cb98b636a65e6cd38cae212c9fd64a37d
Rachel Dolan <rachel@jdlt.co.uk>
2019-04-17 10:58:58 +0100

    changes to manifest

--- src/sw-epilog.js
+++ src/sw-epilog.js
@@ -1,6 +1,9 @@
 self.addEventListener("install", event => {
-  return event.waitUntil(() => {
+  event.waitUntil(() => {
     const cache = caches.open("tubeStatus");
+    
+    console.log('caches ', caches)
+    console.log('cache ', cache)
     return cache.add(
       "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status"
     );
@@ -8,11 +11,23 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(async function() {
-    try {
-      return await fetch(event.request);
-    } catch (err) {
-      return caches.match(event.request);
-    }
-  }());
+  console.log('in fetch try/catch ', event.request.url)
+   event.respondWith(async () => {
+  //  if(event.request.url.includes('api.tfl.gov.uk')){
+    // try {
+      console.log('making network request ', event.request.url)
+      return await fetch(event.request.url, {mode: 'no-cors'})
+    // } catch (err) {
+    //   console.log('responding with cached data ', event.request.url)
+    //   if(caches.match(event.request.url)) return caches.match(event.request.url)
+    //   console.log('An Error Occurred');
+    // }
+  // }
+  //   try {
+  //     return caches.match(event.request)
+  //   } catch (err) {
+  //     return await fetch(event.request, {mode: 'no-cors'})
+  //   }
+  })
 });
+
